8f13c85a7c1dc711f83ff636092a29bb2a9a02a9
Iori Franco Digital <iorifranco.digital@gmail.com>
2026-01-23 14:35:47 +0000

    Em todas as modais, ao clicar fora delas, não fecha-las
    isto é um compo

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -61,6 +61,7 @@ const SheetContent = React.forwardRef<
     <SheetOverlay />
     <SheetPrimitive.Content
       ref={ref}
+      onPointerDownOutside={(e) => e.preventDefault()}
       className={cn(sheetVariants({ side }), className)}
       {...props}
     >
