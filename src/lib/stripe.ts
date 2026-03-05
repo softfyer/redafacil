6494c6816ed966e6de4d16aaa2950360006108ee
iorifranco.digital <iorifranco.digital@gmail.com>
2026-03-05 12:22:26 +0000

    correçõa do arquivo de integração do stripe para teste de hospedagem em prod

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,10 @@
-
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-04-10',
+// Removemos o "!" e adicionamos um fallback (uma string vazia)
+// Isso permite que o objeto 'stripe' seja criado sem estourar erro no build.
+const key = process.env.STRIPE_SECRET_KEY || '';
+
+export const stripe = new Stripe(key, {
+  apiVersion: '2024-04-10', // Mantendo sua versão original
+  typescript: true,
 });
